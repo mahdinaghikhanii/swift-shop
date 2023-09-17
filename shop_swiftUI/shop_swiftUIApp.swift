a2ffd39e6630b4f85b815b90414671516ab28764
Mahdinaghikhani <mahdinkh7@gmail.com>
2023-09-18 02:02:08 +0330

    Initial Commit

---
+++ shop_swiftUI/shop_swiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  shop_swiftUIApp.swift
+//  shop_swiftUI
+//
+//  Created by mahdi naghikhani on 18/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct shop_swiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
